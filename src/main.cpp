1b4d4db69f1877fa05849f9f451d0122fbc79f04
themichaelcaruso <mcaruso@anchron.com>
2019-09-26 11:32:02 -0400

    Boilerplate

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       Michael                                                   */
+/*    Created:      Fri May 24 2019                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+#include "vex.h"
+
+using namespace vex;
+
+int main() {
+  auto var = 3;
+
+}
+
+namespace vlib {
+  /**
+    * @prog_lang{block|cpp|pro}
+    * @brief Use the motor class to control motor devices.
+  */
+  class motor : public vex::motor {
+      
+  }
+}
